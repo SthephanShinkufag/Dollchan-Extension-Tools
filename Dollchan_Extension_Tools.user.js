139048cfef628a0d272f1024550f049892b60e1a
Dark Daskin <darkdaskin@gmail.com>
2011-10-16 14:57:53 +0400

    Добавлена поддержка автообновления в Scriptish.

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			2011-10-13
+// @version			2011-10-16
 // @namespace		http://www.freedollchan.org/scripts
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
 // @description		Doing some profit for imageboards
+// @updateURL		https://github.com/SthephanShinkufag/Dollchan-Extension-Tools/raw/master/Dollchan_Extension_Tools.meta.js
 // @include			*
 // ==/UserScript==
 
