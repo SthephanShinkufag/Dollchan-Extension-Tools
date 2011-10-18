d53524ca282b75844c179190199c8f62927b9830
Sthephan Shinkufag <straiister@yandex.ru>
2011-10-18 15:22:14 +0300

    Version upd.

--- Dollchan_Extension_Tools.meta.js
+++ Dollchan_Extension_Tools.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name			Dollchan Extension Tools
-// @version			2011-10-17
+// @name  		Dollchan Extension Tools
+// @version			2011-10-18
 // @namespace		http://www.freedollchan.org/scripts
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
