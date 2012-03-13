e28f07a6878e1813863132a08af16ab6b0fb3010
Y0ba <lazwlad@gmail.com>
2012-03-13 07:55:16 +0400

    Fix keyboard navigation

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1326,22 +1326,22 @@ function initKeyNavig() {
 	};
 	doc.onkeydown = function (e) {
 		if(!e) e = window.event;
-		var kc = e.keyCode;
+		var kc = e.keyCode, curTh;
 		if(kIgnore || e.ctrlKey || e.altKey || e.shiftKey
 			|| (kc !== 74 && kc !== 75 && kc !== 77 && kc !== 78 && kc !== 86)) return;
 		$pD(e);
-		if(scrollT) {
-			cPIndex = !scrollP ? pByCnt.indexOf(tByCnt[cTIndex]) : findCurrPost(pByCnt);
-			scrollT = false;
-		}
+		if(scrollT) cPIndex = !scrollP ? pByCnt.indexOf(tByCnt[cTIndex]) : findCurrPost(pByCnt);
 		if(scrollP) {
-			cTIndex = (pByCnt[cPIndex] || []).isOp
-				? tByCnt.indexOf(pByCnt[cPIndex]) : findCurrPost(tByCnt);
-			scrollP = false;
+			if(!scrollT && pByCnt[cPIndex].isOp) cTIndex = curTh = tByCnt.indexOf(pByCnt[cPIndex]);
+			else {
+				cTIndex = findCurrPost(tByCnt);
+				curTh = cTIndex + 1;
+			}
 		}
+		scrollP = scrollT = false;
 		if(kc === 86) {
 			if(TNum) showQuickReply(pByCnt[cPIndex]);
-			else window.open(getThrdUrl(host, brd, tByCnt[cTIndex < 0 ? 0 : cTIndex].Num), '_blank');
+			else window.open(getThrdUrl(host, brd, tByCnt[curTh].Num), '_blank');
 			return;
 		}
 		scrScroll = true;
