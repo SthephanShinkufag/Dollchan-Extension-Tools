cb72d89ee0e70a6014ed99a16089cfa8b7d323ae
Sthephan Shinkufag <straiister@yandex.ru>
2012-03-13 22:03:57 +0200

    Switch update links to 'stable' branch

--- Dollchan_Extension_Tools.meta.js
+++ Dollchan_Extension_Tools.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			12.3.13.5
+// @version			12.3.13.6
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
 // @description		Doing some profit for imageboards
-// @icon			https://github.com/SthephanShinkufag/Dollchan-Extension-Tools/raw/master/Icon.png
-// @updateURL		https://github.com/SthephanShinkufag/Dollchan-Extension-Tools/raw/master/Dollchan_Extension_Tools.meta.js
+// @icon			https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/stable/Icon.png
+// @updateURL		https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/stable/Dollchan_Extension_Tools.meta.js
 // @include			*
 // ==/UserScript==
