4e367465f11703384a69c83ca9b17f7087be2ca4
Sthephan Shinkufag <straiister@yandex.ru>
2012-04-30 11:39:18 +0300

    Fix for manual updating with 'Never' option

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			12.4.29.3
+// @version			12.4.30.0
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
@@ -13,7 +13,7 @@
 (function (scriptStorage) {
 'use strict';
 var defaultCfg = {
-	'version':	'2012.04.29.3',
+	'version':	'2012.04.30.0',
 	'lang':		0,		// script language [0=ru, 1=en]
 	'sstyle':	0,		// script elements style [0=gradient blue, 1=solid grey]
 	'spells':	0,		// hide posts by magic spells
@@ -5497,7 +5497,6 @@ function hideByWipe(post) {
 ==============================================================================*/
 
 function checkForUpdates(force, onlyIfNew, fn) {
-	if(Cfg.supdint === 6) return;
 	var t = +(new Date()).getTime(), updInt, day = 2*1000*60*60*24;
 	switch(Cfg.supdint) {
 	case 0:
@@ -6126,7 +6125,7 @@ function doScript() {
 	scriptCSS();
 	Log('scriptCSS');
 	endTime = (new Date()).getTime() - initTime;
-	if(Cfg.enupd !== 0) {
+	if(Cfg.enupd !== 0 && Cfg.supdint !== 6) {
 		checkForUpdates(false, true, function(html) {
 			$alert(html);
 		});
