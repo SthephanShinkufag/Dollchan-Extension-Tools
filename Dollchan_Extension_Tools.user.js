f84ea1c0febb4a3e83ea850f7e6c4df7eb14bc93
Sthephan Shinkufag <straiister@yandex.ru>
2012-05-02 21:54:31 +0300

    Fix board detection in post preview

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			12.5.2.5
+// @version			12.5.2.6
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
@@ -13,7 +13,7 @@
 (function (scriptStorage) {
 'use strict';
 var defaultCfg = {
-	'version':	'12.5.2.5',
+	'version':	'12.5.2.6',
 	'lang':		0,		// script language [0=ru, 1=en]
 	'sstyle':	0,		// script elements style [0=gradient blue, 1=solid grey]
 	'spells':	0,		// hide posts by magic spells
@@ -4065,7 +4065,7 @@ function funcPostPreview(post, pNum, parent, e, txt) {
 }
 
 function showPostPreview(e) {
-	var b = this.pathname.match(/^\/*(.*?)\/*(?:res|thread-|index|$)/)[1],
+	var b = this.pathname.match(/^\/*(.*?)\/*(?:res|thread-|index|\d+|$)/)[1],
 		tNum = (this.pathname.match(/[^\/]+\/[^\d]*(\d+)/) || [,0])[1],
 		pNum = (this.hash.match(/\d+/) || [tNum])[0],
 		post = pByNum[pNum] || importPreview(b, pNum),
