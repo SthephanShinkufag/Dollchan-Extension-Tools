7c5316126941c955e78a720a4c131617edf569f0
Sthephan Shinkufag <straiister@yandex.ru>
2012-05-15 22:32:10 +0300

    Fix autoxpand images in ajaxed posts

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			12.5.15.1
+// @version			12.5.15.2
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
@@ -4207,7 +4207,7 @@ function addPostFunc(post) {
 		mergeHidden(post);
 	}
 	if(isExpImg) {
-		expandAllPostImg(post, false);
+		expandAllPostImg(post, null);
 	}
 }
 
