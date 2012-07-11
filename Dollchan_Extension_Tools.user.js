bc9c41981172bb0b976481da6c0b7d08255ea393
Y0ba <lazwlad@gmail.com>
2012-07-11 23:54:48 +0400

    Revert some changes

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -6021,7 +6021,8 @@ function scriptCSS() {
 		x += 'img[id^="translate_button"], img[src$="button-expand.gif"], img[src$="button-close.gif"]' + (liteMode ? ', div[id^="disclaimer"]' : '') + ' { display: none !important; }\
 			div[id^="Wz"] { z-index: 10000 !important; }\
 			div[id^="DESU_hidThr_"] { margin-bottom: ' + (!TNum ? '7' : '2') + 'px; }\
-			.file_reply + .DESU_ytObj { margin: 5px 20px 5px 5px;  float: left; }';
+			.file_reply + .DESU_ytObj { margin: 5px 20px 5px 5px;  float: left; }\
+			.DESU_ytObj + div { clear: left; }';
 	} else if(aib._420) {
 		x += '.opqrbtn, .qrbtn, .ignorebtn, .hidethread, noscript { display: none; }\
 			div[id^="DESU_hidThr_"] { margin: 1em 0; }';
@@ -6216,7 +6217,7 @@ function getNavigator() {
 		nav.animEnd =
 			nav.WebKit ? 'webkitAnimationEnd' :
 			nav.Opera ? 'oAnimationEnd' :
-			'animationEnd';
+			'animationend';
 		nav.animEvent = function(el, Fn) {
 			el.addEventListener(nav.animEnd, function aEvent() {
 				this.removeEventListener(nav.animEnd, aEvent, false);
