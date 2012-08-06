461d577e00d4c549d72301f6b27ecdba868c07ff
Y0ba <lazwlad@gmail.com>
2012-08-06 17:39:38 +0400

    Save hash alone with spells

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -361,8 +361,8 @@ pr = {}, dForm, oeForm, dummy, postWrapper = false, refMap = [],
 Pviews = {deleted: [], ajaxed: {}, current: null, outDelay: null},
 Favico = {href: '', delay: null, focused: false},
 Audio = {enabled: false, el: null, repeat: false, running: false},
-pSpells = {}, tSpells = {}, oSpells = {}, spellsList = [],
-oldTime, endTime, timeLog = '', dTime, crc32table,
+pSpells, tSpells, oSpells, spellsList, spellsHash,
+oldTime, endTime, timeLog = '', dTime,
 ajaxInterval, lCode, hideTubeDelay, quotetxt = '', liteMode = false, isExpImg = false;
 
 
@@ -580,7 +580,9 @@ function $toRegExp(str) {
 
 function $isEmpty(obj) {
 	for(var i in obj) {
-		return false;
+		if(obj.hasOwnProperty(i)) {
+			return false;
+		}
 	}
 	return true;
 }
@@ -741,6 +743,28 @@ function getPrettyJSON(obj, indent) {
     return sJSON += '\n' + indent + (isArr ? ']' : '}');
 }
 
+function ELFHash(arr, len) {
+    for(var h = 0, g, i = 0; i < len; ++i) {
+        h = (h << 4) + arr[i];
+        if(g = h & 0xF0000000) {
+            h ^= g >>> 24;
+        }
+        h &= ~g;
+    }
+    return h;
+}
+
+function ELFHashStr(str) {
+    for(var h = 0, g, i = 0, len = str.length; i < len; ++i) {
+        h = (h << 4) + str.charCodeAt(i);
+        if(g = h & 0xF0000000) {
+            h ^= g >>> 24;
+        }
+        h &= ~g;
+    }
+    return h;
+}
+
 
 /*==============================================================================
 								STORAGE / CONFIG
@@ -811,9 +835,22 @@ function getStoredObj(id, def) {
 }
 
 function saveSpells(val) {
+	spellsHash = ELFHashStr(val);
 	spellsList = val.split('\n');
-	setStored('DESU_Spells_' + aib.dm, val);
+	setStored('DESU_Spells_' + aib.dm, JSON.stringify([spellsHash, spellsList]));
 	initSpells();
+}
+
+function readSpells() {
+	var spells = getStored('DESU_Spells_' + aib.dm), sObj;
+	try {
+		sObj = JSON.parse(spells);
+		spellsHash = sObj[0];
+		spellsList = sObj[1];
+		initSpells();
+	} catch(e) {
+		saveSpells(spells || '');
+	}
 }
 
 /** @constructor */
@@ -882,7 +919,9 @@ function readCfg() {
 	if(Cfg['correctTime']) {
 		dTime = new dateTime(Cfg['timePattern'], Cfg['timeOffset']);
 	}
-	saveSpells(getStored('DESU_Spells_' + aib.dm) || '');
+	if(Cfg['hideBySpell']) {
+		readSpells();
+	}
 }
 
 function saveCfg(id, val) {
@@ -894,27 +933,6 @@ function saveCfg(id, val) {
 
 function toggleCfg(id) {
 	saveCfg(id, !Cfg[id] ? 1 : 0);
-}
-
-function getVisib(pNum) {
-	var key = nav.isCookie ? pByNum[pNum].Count : brd + pNum;
-	return key in Visib ? Visib[key] : null;
-}
-
-function getPostVisib(post) {
-	var pNum = post.Num;
-	post.Vis = getVisib(pNum);
-	if(post.isOp) {
-		if(hThrds[brd] && (
-			nav.isCookie && hThrds[brd].indexOf(pNum) >= 0
-				|| !nav.isCookie && hThrds[brd][pNum] !== undefined
-		)) {
-			post.Vis = 0;
-		} else if(post.Vis === 0) {
-			Visib[brd + pNum] = null;
-			post.Vis = null;
-		}
-	}
 }
 
 function readPostsVisib() {
@@ -944,7 +962,21 @@ function readPostsVisib() {
 		}
 	}
 	readHiddenThreads();
-	Posts.forEach(getPostVisib);
+	Posts.forEach(function(post) {
+		var pNum = post.Num;
+		post.Vis = Visib[nav.isCookie ? pByNum[pNum].Count : brd + pNum];
+		if(post.isOp) {
+			if(hThrds[brd] && (
+				nav.isCookie && hThrds[brd].indexOf(pNum) >= 0
+					|| !nav.isCookie && hThrds[brd][pNum] !== undefined
+			)) {
+				post.Vis = 0;
+			} else if(post.Vis === 0) {
+				Visib[brd + pNum] = null;
+				post.Vis = null;
+			}
+		}
+	});
 }
 
 function savePostsVisib() {
@@ -1316,8 +1348,7 @@ function cfgTab(name, id) {
 				}
 				el.className = 'DESU_cfgBody';
 				if(id === 'cfgFilters') {
-					spellsList = getStored('DESU_Spells_' + aib.dm).split('\n');
-					initSpells();
+					readSpells();
 					$id('DESU_spellEdit').value = spellsList.join('\n');
 				}
 			}
@@ -4521,7 +4552,7 @@ function newPost(thr, post, pNum, i) {
 			post.Text.substring(0, 70).replace(/\s+/g, ' ');
 		Threads.push(post);
 	}
-	post.Vis = getVisib(post.Num);
+	post.Vis = Visib[nav.isCookie ? pByNum[pNum].Count : brd + pNum];
 	addPostButtons(post);
 	if(Cfg['expandPosts'] && !TNum) {
 		expandPost(post);
@@ -5301,20 +5332,6 @@ function hideBySameText(post) {
 
 /*-------------------------Hide posts with similar images---------------------*/
 
-function getCRC32table() {
-	crc32table = new Uint32Array([0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3, 0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7, 0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5, 0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B, 0x35B5A8FA, 0x42B2986C, 0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59, 0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F, 0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D, 0x76DC4190, 0x01DB7106, 0x98D220BC, 0xEFD5102A, 0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433, 0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x086D3D2D, 0x91646C97, 0xE6635C01, 0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E, 0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457, 0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65, 0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB, 0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9, 0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F, 0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81, 0xB7BD5C3B, 0xC0BA6CAD, 0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683, 0xE3630B12, 0x94643B84, 0x0D6D6A3E, 0x7A6A5AA8, 0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1, 0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7, 0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5, 0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B, 0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79, 0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F, 0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D, 0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A, 0x9C0906A9, 0xEB0E363F, 0x72076785, 0x05005713, 0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38, 0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21, 0x86D3D2D4, 0xF1D4E242, 0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777, 0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45, 0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB, 0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9, 0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF, 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D]);
-}
-
-function getCRC32(data, length) {
-	if(!crc32table) {
-		getCRC32table();
-	}
-	for(var i = 0, crc = -1; i < length; i++) {
-		crc = (crc >>> 8) ^ crc32table[(crc ^ data[i]) & 0xFF];
-	}
-	return crc ^ (-1);
-}
-
 function prepareImgHash(data, oldw, oldh) {
 	var i, j, l, c, t, u, tmp = oldw * oldh,
 		newh = 8,
@@ -5322,10 +5339,9 @@ function prepareImgHash(data, oldw, oldh) {
 		levels = 2,
 		areas = 256 / levels,
 		values = 256 / (levels - 1),
-		rv = new Uint8Array(newh * neww),
-		des = new Uint8Array(tmp);
+		rv = new Uint8Array(newh * neww);
 	for(i = 0, j = 0; i < tmp; i++, j += 4) {
-		des[i] = data[j] * 0.3 + data[j + 1] * 0.59 + data[j + 2] * 0.11;
+		data[i] = data[j] * 0.3 + data[j + 1] * 0.59 + data[j + 2] * 0.11;
 	}
 	for(i = 0; i < newh; i++) {
 		for(j = 0; j < neww; j++) {
@@ -5345,10 +5361,10 @@ function prepareImgHash(data, oldw, oldh) {
 				c = oldw - 2;
 			}
 			t = tmp - c;
-			tmp = values * (((des[l * oldw + c] * ((1 - t) * (1 - u)) +
-				des[l * oldw + c + 1] * (t * (1 - u)) +
-				des[(l + 1) * oldw + c + 1] * (t * u) +
-				des[(l + 1) * oldw + c] * ((1 - t) * u)) / areas) >> 0);
+			tmp = values * (((data[l * oldw + c] * ((1 - t) * (1 - u)) +
+				data[l * oldw + c + 1] * (t * (1 - u)) +
+				data[(l + 1) * oldw + c + 1] * (t * u) +
+				data[(l + 1) * oldw + c] * ((1 - t) * u)) / areas) >> 0);
 			if(tmp > 255) tmp = 255;
 			rv[i * newh + j] = tmp;
 		}
@@ -5359,8 +5375,8 @@ function prepareImgHash(data, oldw, oldh) {
 function getImgHash(post) {
 	var w, h, cnv, ctx,
 		img = post.Img[0];
-	if(img.crc32) {
-		return img.crc32;
+	if(img.hash) {
+		return img.hash;
 	}
 	cnv = $id('DESU_canvas') || doc.body.appendChild($new('canvas', {
 		'id': 'DESU_canvas',
@@ -5370,7 +5386,7 @@ function getImgHash(post) {
 	h = cnv.height = img.height;
 	ctx = cnv.getContext('2d');
 	ctx.drawImage(img, 0, 0);
-	return img.crc32 = getCRC32(prepareImgHash(ctx.getImageData(0, 0, w, h).data, w, h), 8 * 8);
+	return img.hash = ELFHash(prepareImgHash(ctx.getImageData(0, 0, w, h).data, w, h), 8 * 8);
 }
 
 /*==============================================================================
@@ -6757,7 +6773,7 @@ function tryToParse(node) {
 }
 
 function replaceDelform(el) {
-	if(aib.fch || aib.krau || dTime || Cfg['hideBySpell'] && oSpells.rep[0]) {
+	if(aib.fch || aib.krau || dTime || (oSpells && oSpells.rep[0])) {
 		var txt = el.innerHTML;
 		if(dTime) {
 			txt = dTime.init(txt).fix(txt, null);
