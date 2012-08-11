2bf8c0a7327da752b9b39299746db5b64527f6e6
Sthephan Shinkufag <straiister@yandex.ru>
2012-08-11 23:49:20 +0300

    Version update

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			12.7.20.0
+// @version			12.8.11.0
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
@@ -12,7 +12,7 @@
 
 'use strict';
 var defaultCfg = {
-	'version':	'12.7.20.0',
+	'version':	'12.8.11.0',
 	'language':		0,		// script language [0=ru, 1=en]
 	'hideBySpell':	0,		// hide posts by spells
 	'hideByWipe':	1,		// antiwipe detectors:
