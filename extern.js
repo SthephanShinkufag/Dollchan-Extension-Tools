fb309c98252e3b291af45857609d8a70fcfa173d
Sthephan Shinkufag <straiister@yandex.ru>
2012-12-10 23:54:15 +0200

    Version update

--- extern.js
+++ extern.js
@@ -3,8 +3,11 @@
  */
 
 /** @return {String} */ Document.prototype.getSelection = function() {};
+
 /** @type {Object} */ Window.opera;
-/** @type {Object} */ Window.opera.scriptStorage; 
+
+/** @type {Object} */ Window.opera.scriptStorage;
+
 /** @return {String} */ Window.opera.version = function() {};
 
 /**
@@ -12,6 +15,7 @@
  * @return {Node}
  */
 Document.prototype.implementation.createHTMLDocument = function(html) {};
+
 /**
  * @param {String} string
  * @param {number=} start_index
@@ -19,6 +23,14 @@ Document.prototype.implementation.createHTMLDocument = function(html) {};
  * @nosideeffects
  */
 String.prototype.contains = function(string, start_index) {};
+
+/**
+ * @param {String} string
+ * @return {Boolean}
+ * @nosideeffects
+ */
+String.prototype.startsWith = function(string) {};
+
 /**
  * @param {Function|string} callback
  * @param {number} delay
@@ -26,14 +38,9 @@ String.prototype.contains = function(string, start_index) {};
  * @return {number}
  */
 function setTimeout(callback, delay, var_args) {};
-/**
- * @constructor
- * @return {Object}
- * @nosideeffects
- */
-function MozBlobBuilder() {};
 
-Window.JSON = {};
+/** @type {Object} */ Window.JSON;
+
 /**
  * @param {string} text
  * @param {(function(string, *) : *)=} opt_reviver
@@ -41,7 +48,12 @@ Window.JSON = {};
  */
 Window.JSON.parse = function(text, opt_reviver) {};
 
+Window.Worker.prototype.postMessage = function() {};
+
+Window.prototype._postMessage = function() {};
+
 /** @type {String} */ XMLHttpRequest.prototype.finalUrl;
+
 var localStorage = {}, sessionStorage = {}, self = {};
 
 /**
@@ -49,32 +61,38 @@ var localStorage = {}, sessionStorage = {}, self = {};
  * @return {undefined}
  */
 function GM_log(text) {};
+
 /**
  * @param {String} text
  * @return {undefined}
  */
 Window.GM_log = function(text) {};
+
 /**
  * @param {Object} obj
  * @return {undefined}
  */
 function GM_xmlhttpRequest(obj) {};
+
 /**
  * @param {Object} obj
  * @return {undefined}
  */
 Window.GM_xmlhttpRequest = function(obj) {};
+
 /**
  * @param {String} name
  * @param {String} value
  * @return {undefined}
  */
 function GM_setValue(name, value) {};
+
 /**
  * @param {String} name
  * @return {String}
  */
 function GM_getValue(name) {};
+
 /**
  * @param {String} url
  * @param {Boolean} loadInBackground
@@ -82,6 +100,7 @@ function GM_getValue(name) {};
  * @return {undefined}
  */
 function GM_openInTab(url, loadInBackground, reuseTab) {};
+
 /**
  * @param {String} path
  * @param {Node} root
