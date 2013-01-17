e8f55bf610fb2f88caf378a2797e02e5c4681126
Sthephan Shinkufag <straiister@yandex.ru>
2013-01-17 14:29:10 +0200

    Fix initialization for config in localstorage

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -824,10 +824,10 @@ function readCfg() {
 			for(var i in comCfg['global']) {
 				Cfg[i] = comCfg['global'][i];
 			}
-			Cfg['captchaLang'] = aib.ru ? 2 : 1;
-			Cfg['timePattern'] = Cfg['timeOffset'] = '';
-			Cfg['correctTime'] = 0;
 		}
+		Cfg['captchaLang'] = aib.ru ? 2 : 1;
+		Cfg['timePattern'] = Cfg['timeOffset'] = '';
+		Cfg['correctTime'] = 0;
 	}
 	Cfg.__proto__ = defaultCfg;
 	if(!nav.isBlob) {
