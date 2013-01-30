b3bde9eb3ac4231f87b96fa1aca652d536db0047
Y0ba <lazwlad@gmail.com>
2013-01-30 05:41:43 +0400

    Fix abu css

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -6663,9 +6663,10 @@ function scriptCSS() {
 	} else {
 		x+= '.de-post-hid > .de-ppanel ~ * { display: none !important; }'
 		if(aib.abu) {
-			x += '.ABU_refmap, .postpanel, #CommentToolbar, #usrFlds + tbody > tr:first-child, #postform > div:nth-child(2), #BottomNormalReply, body > center { display: none !important; }\
+			x += '.ABU_refmap, .postpanel, #CommentToolbar, #usrFlds + tbody > tr:first-child, #postform > div:nth-child(2), #BottomNormalReply, body > center, .logo + div { display: none !important; }\
 				#de-txt-panel { font-size: 16px !important; }\
-				.de-abtn { transition: none; }';
+				.de-abtn { transition: none; }\
+				.reflink:before { content: none !important; }';
 		} else if(aib.nul) {
 			x += '#postform nobr, .replieslist, #captcha_status, .postnode + a, .postblock + td > small, .content-background > hr, span[style="float: right;"] { display: none !important; }\
 				.ui-wrapper { position: static !important; margin: 0 !important; overflow: visible !important; }\
