39df51756869e8a696c6d0300b27541900a55d5a
Y0ba <lazwlad@gmail.com>
2013-11-03 06:56:31 +0400

    Update meta file

--- Dollchan_Extension_Tools.meta.js
+++ Dollchan_Extension_Tools.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name			Dollchan Extension Tools
-// @version			13.9.19.0
+// @version			13.11.03.0
 // @namespace		http://www.freedollchan.org/scripts/*
 // @author			Sthephan Shinkufag @ FreeDollChan
 // @copyright		(C)2084, Bender Bending Rodriguez
 // @description		Doing some profit for imageboards
-// @icon			https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/stable/Icon.png
-// @updateURL		https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/stable/Dollchan_Extension_Tools.meta.js
+// @icon			https://raw.github.com/Y0ba/Dollchan-Extension-Tools/master/Icon.png
+// @updateURL		https://raw.github.com/Y0ba/Dollchan-Extension-Tools/master/Dollchan_Extension_Tools.meta.js
 // @include			*
 // ==/UserScript==
