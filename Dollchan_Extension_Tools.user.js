d76304a8c04fe998418278ccc7e3ea8ba30d4e7f
Sthephan Shinkufag <sthephan.shi@gmail.com>
2014-01-14 16:07:23 +0200

    New thread updater animation

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -5454,12 +5454,14 @@ function scriptCSS() {
 			@keyframes de-post-close-bl { to { transform: translate(-50%,50%) scale(0); opacity: 0; } }\
 			@keyframes de-post-close-tr { to { transform: translate(50%,-50%) scale(0); opacity: 0; } }\
 			@keyframes de-post-close-br { to { transform: translate(50%,50%) scale(0); opacity: 0; } }\
+			@keyframes de-post-new { from { transform: translateX(-100%); } }\
 			.de-pview-anim { animation-duration: .2s; animation-timing-function: ease-in-out; animation-fill-mode: both; }\
 			.de-open { animation: de-open .7s ease-out both; }\
 			.de-close { animation: de-close .7s ease-in both; }\
 			.de-blink { animation: de-blink .7s ease-in-out both; }\
-			.de-cfg-open { animation: de-cfg-open .2s ease-out backwards; }\
-			.de-cfg-close { animation: de-cfg-close .2s ease-in both; }';
+			.de-cfg-open { animation: de-cfg-open .3s ease-out backwards; }\
+			.de-cfg-close { animation: de-cfg-close .3s ease-in both; }\
+			.de-post-new { animation: de-post-new .2s ease-out both; }';
 	}
 
 	// Embedders
@@ -8264,9 +8266,9 @@ Thread.prototype = {
 		aib.appendPost(wrap, parent);
 		if(TNum && Cfg['animation']) {
 			nav.animEvent(post.el, function(node) {
-				node.classList.remove('de-open');
+				node.classList.remove('de-post-new');
 			});
-			post.el.classList.add('de-open');
+			post.el.classList.add('de-post-new');
 		}
 		youTube.parseLinks(post);
 		if(Cfg['imgSrcBtns']) {
