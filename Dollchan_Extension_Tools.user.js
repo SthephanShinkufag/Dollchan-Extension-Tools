397be59da49847592362062fe1b617cc627c57ab
Y0ba <lazwlad@gmail.com>
2014-10-11 05:51:47 +0400

    Fix makaba

--- Dollchan_Extension_Tools.user.js
+++ Dollchan_Extension_Tools.user.js
@@ -10075,8 +10075,8 @@ function getImageBoard(checkDomains, checkOther) {
 			} },
 			hasPicWrap: { value: true },
 			init: { value: function () {
-				return true;
 				$script('window.FormData = void 0;');
+				doc.body.insertAdjacentHTML('afterbegin', '<div id="jcaptcha"></div>');
 				var el = $q('tr:not([class])', doc.body);
 				if (!el) {
 					return;
