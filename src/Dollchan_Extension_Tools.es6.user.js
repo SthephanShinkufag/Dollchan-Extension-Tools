b10b563a622338058f2b2cf74f9934137cc392b4
SthephanShinkufag <sthephan.shi@gmail.com>
2015-02-26 01:51:17 +0200

    Add es6 file into script updater

--- src/Dollchan_Extension_Tools.es6.user.js
+++ src/Dollchan_Extension_Tools.es6.user.js
@@ -11861,7 +11861,10 @@ function checkForUpdates(isForce) {
 					i++;
 				}
 				if(isUpd) {
-					resolve('<a style="color: blue; font-weight: bold;" href="https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/master/Dollchan_Extension_Tools.user.js">' +
+					resolve('<a style="color: blue; font-weight: bold;" href="' +
+						'https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/master/' +
+						(nav.Firefox && 'Promise' in window ? 'src/Dollchan_Extension_Tools.es6.user.js">' :
+							'Dollchan_Extension_Tools.user.js">') +
 					Lng.updAvail[lang] + '</a>');
 				} else if(isForce) {
 					resolve(Lng.haveLatest[lang]);
