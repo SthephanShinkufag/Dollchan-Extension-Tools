329830e7814186d0312d5eff935ce2b6ea570bac
Y0ba <lazwlad@gmail.com>
2015-06-10 06:27:23 +0300

    Update version

--- src/Dollchan_Extension_Tools.es6.user.js
+++ src/Dollchan_Extension_Tools.es6.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Dollchan Extension Tools
-// @version         15.4.20.0
+// @version         15.6.10.0
 // @namespace       http://www.freedollchan.org/scripts/*
 // @author          Sthephan Shinkufag @ FreeDollChan
 // @copyright       (c) 2015 Dollchan Extension Tools Team. See the LICENSE file for license rights and limitations (MIT).
