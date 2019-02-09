f2d95098d4d9eccf9fbd316efc25b296278418a5
SthephanShi <sthephan.shi@gmail.com>
2019-02-09 22:57:59 +0200

    Fix for security vulnerability (alert in github)

--- src/Dollchan_Extension_Tools.es6.user.js
+++ src/Dollchan_Extension_Tools.es6.user.js
@@ -31,7 +31,7 @@
 'use strict';
 
 const version = '19.1.16.0';
-const commit = 'bd8988e';
+const commit = 'c0f2078';
 
 /* ==[ DefaultCfg.js ]========================================================================================
                                                 DEFAULT CONFIG
@@ -18038,7 +18038,7 @@ function scriptCSS() {
 	.de-list { padding-top: 4px; }
 	.de-list::before { content: "\u25CF"; margin-right: 4px; }
 	.de-logo { display: inline-block; margin-right: 10px; fill: inherit; color: #F5F5F5; border-radius: 80px 0 0 0; }
-	.de-logo > svg { width: 156px; }
+	.de-logo > svg { width: 144px; height: 144px; }
 	.de-menu { padding: 0 !important; margin: 0 !important; width: auto !important; min-width: 0 !important; z-index: 10002; border: 1px solid grey !important; text-align: left; }
 	.de-menu-item { display: block; padding: 3px 10px; color: inherit; text-decoration: none; font: 13px arial; white-space: nowrap; cursor: pointer; }
 	.de-menu-item:hover { background-color: #222; color: #fff; }
--- src/modules/Wrap.js
+++ src/modules/Wrap.js
@@ -6,7 +6,7 @@
 'use strict';
 
 const version = '19.1.16.0';
-const commit = 'bd8988e';
+const commit = 'c0f2078';
 
 /* ==[ DefaultCfg.js ]== */
 /* ==[ Localization.js ]== */
