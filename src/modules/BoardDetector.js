17c64b30c6cda97b86c48ff3d79906cc394c880e
Bernd-kun <127719428+Bernd-kun@users.noreply.github.com>
2023-03-13 06:21:33 +0000

    Compatibility with some vanilla Kohlchan settings
    Some vanilla Kohlchan settings (unix timestamps, gallery mode) require that the "imgLink" class still be present. As long as that's the case, they work fine and don't interfere with Dollchan functionality.

--- src/modules/BoardDetector.js
+++ src/modules/BoardDetector.js
@@ -1732,7 +1732,7 @@ function getImageBoard(checkDomains, checkEngines) {
 				deWindow.location.reload();
 				return true;
 			}
-			$Q('.imgLink').forEach(el => (el.className = 'de-img-link'));
+			$Q('.imgLink').forEach(el => (el.className = 'de-img-link imgLink'));
 			return super.init();
 		}
 	}
