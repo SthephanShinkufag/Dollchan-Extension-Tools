7abaaf45669a161681b7ea9ef27a32231253b588
Odilitime <janesmith@airmail.cc>
2026-04-26 05:44:34 +0000

    modules: fix issues in modules (BoardDetector.js)
    Iteration 1
    
    prr-fix:prrc_kwdoactzas67vtqq

--- src/modules/BoardDetector.js
+++ src/modules/BoardDetector.js
@@ -1538,7 +1538,7 @@ function getImageBoard(checkDomains, checkEngines) {
 			this.formParent = 'thread';
 			this.hasCatalog = true;
 			this.multiFile = true;
-			this.qDelForm = 'form[action*="/forms/board/"][action$="/actions"]:has(.thread)';
+			this.qDelForm = 'form[action*="/forms/board/"][action$="/actions"]';
 			this.qDelBtn = 'input[type="submit"][value="Submit"]';
 			this.qDelPassw = '#password, input[name="postpassword"]';
 			this.qForm = '#postform.form-post, form.form-post[action$="post"]';
@@ -1626,6 +1626,15 @@ function getImageBoard(checkDomains, checkEngines) {
 				return;
 			}
 			super.parseURL();
+		}
+		getDelForm() {
+			const forms = $Q(this.qDelForm);
+			for(let i = 0; i < forms.length; ++i) {
+				if($q('.thread', forms[i])) {
+					return forms[i];
+				}
+			}
+			return null;
 		}
 		updateSubmitBtn(el) {
 			el.value = Lng.reply[lang];
